5e465d457b876c687daf1a5e4f9cfcbdbcdeee72
Ouzin <Ouzinvw@live.com>
2022-09-23 19:18:37 -0400

    Completed 3rd party express middleware activation

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ const _dirname = dirname(fileURLToPath(import.meta.url));
 // instantiate app-server
 const app = express();
 
+// Setup ViewEngine EJS
+app.set('views', path.join(_dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(_dirname, '../public')));
+app.use(session({
+    secret: 'MySecret',
+    saveUninitialized: false,
+    resave: false
+}));
+
 // custom middleware
 function helloWorld(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');

4fc37c7f005b81f102d45e2fefba0f9c08fcae48
Ouzin <Ouzinvw@live.com>
2022-09-23 20:53:39 -0400

    Added 3rd party CSS and scripts (Bootleg + Fonts)

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,5 +1,23 @@
 // Index controller
 
-export function helloWorld(req, res, next) {
-    res.render('index', { title: 'Home' });
+import { render } from "ejs";
+
+export function displayHomePage(req, res, next) {
+    res.render('index', { title: 'Home', page: 'home' });
 };
+
+export function displayContactPage(req, res, next) {
+    res.render('index', { title: 'Contact', page: 'contact' });
+}
+
+export function displayServicesPage(req, res, next) {
+    res.render('index', { title: 'Services', page: 'services' });
+}
+
+export function displayProjectsPage(req, res, next) {
+    res.render('index', { title: 'Projects', page: 'projects' });
+}
+
+export function displayAboutPage(req, res, next) {
+    res.render('index', { title: 'About', page: 'about' });
+}
--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,12 +1,16 @@
 // Index Routes
 import { Router } from 'express';
-import { helloWorld } from '../controllers/index.controller.server.js';
+import { displayHomePage, displayAboutPage, displayContactPage, displayServicesPage, displayProjectsPage } from '../controllers/index.controller.server.js';
 
 // Instantiating the router
 const router = Router();
 
 // Middleware to connect application
-router.get('/', helloWorld);
-router.get('/hello', helloWorld);
+router.get('/', displayHomePage);
+router.get('/home', displayHomePage);
+router.get('/about', displayAboutPage);
+router.get('/projects', displayProjectsPage);
+router.get('/contact', displayContactPage);
+router.get('/services', displayServicesPage);
 
 export default router;
